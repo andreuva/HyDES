41c730c427ad32d58ffee4fcb59b7437fa712613
andreuva <andres.vicente.arevalo@gmail.com>
2021-12-09 10:34:03 +0000

    added quiver and changed params

--- Python/display.py
+++ Python/display.py
@@ -18,7 +18,16 @@ class display:
         if not os.path.exists(self.savepath):
             os.mkdir(self.savepath)
 
-        self.simfig, self.sim_ax_list = plt.subplots(2, 2, figsize=(6,6))
+        if params["plotType"] == "quiver":
+            self.nrows = 1
+            self.ncols = 1
+            self.skip = (slice(None,None,10),slice(None,None,10))
+        else:
+            self.nrows = 2
+            self.ncols = 2
+
+
+        self.simfig, self.sim_ax_list = plt.subplots(self.nrows, self.ncols, figsize=(6,6))
         plt.rcParams['image.cmap'] = 'plasma'
         self.simfig.suptitle('2D Hydrodinamic Simulation')
 
@@ -36,10 +45,9 @@ class display:
         self.update(domain, state)
 
     def update(self, domain, state, time=0):
-        # Plot the state
-        ax_list = self.sim_ax_list.ravel()
-        
+        # Plot the state        
         if self.plot_type == "contourf":
+            ax_list = self.sim_ax_list.ravel()
             ax_list[0].set_title('Density')
             ax_list[0].contourf(domain.x,  domain.y, (state.Um-state.Um00)/state.Um00, 20, vmin=-self.umamp , vmax=self.umamp)
             ax_list[1].set_title('Pressure')
@@ -50,6 +58,7 @@ class display:
             ax_list[3].contourf(domain.x,  domain.y, state.vy/state.cs00, 20, vmin=self.vmin, vmax=self.vmax)
 
         elif self.plot_type == "color":
+            ax_list = self.sim_ax_list.ravel()
             ax_list[0].set_title('Density')
             ax_list[0].imshow((state.Um-state.Um00)/state.Um00,
                             extent=[domain.xmin,domain.xmax, domain.ymin,domain.ymax],
@@ -66,11 +75,21 @@ class display:
             ax_list[3].imshow(state.vy/state.cs00,
                           extent=[domain.xmin,domain.xmax, domain.ymin,domain.ymax],
                           vmin=self.vmin, vmax=self.vmax)
+        elif self.plot_type == "quiver":
+            self.sim_ax_list.clear()
+            self.sim_ax_list.set_title('Density with velocity')
+            self.sim_ax_list.quiver(domain.xmesh[self.skip], domain.ymesh[self.skip],
+                                    (state.vx/state.cs00)[self.skip], (state.vy/state.cs00)[self.skip],
+                                    ((state.pres-state.p00)/state.p00)[self.skip], angles='xy', scale_units='xy', scale=1.)
+            self.sim_ax_list.set_xlabel('x')
+            self.sim_ax_list.set_ylabel('y')
+            self.sim_ax_list.set_aspect('equal')
         else:
             raise ValueError("Plot type not recognized")
 
-        for ax in ax_list:
-            ax.label_outer()
+        if self.plot_type != "quiver":
+            for ax in ax_list:
+                ax.label_outer()
         
         plt.draw()
         plt.pause(0.00001)
