1d9326e533a4eb815fc309533e7355916c6f7e00
andreuva <andres.vicente.arevalo@gmail.com>
2022-01-06 15:13:05 +0100

    change version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 setup(
 
     name = "hydes",
-    version = "0.0.4",
+    version = "0.0.5",
     author = "Andres Vicente Arevalo",
     author_email = "andres.vicente.arevalo@gmail.com",
     description = "Hydrodinamical equation solver",
